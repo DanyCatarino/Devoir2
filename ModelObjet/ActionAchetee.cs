456cf15b2b13c3c71324abdaa4616cab35838fed
DanyCatarino <catarino.dany@gmail.com>
2019-10-02 14:44:36 +0200

    Finalisation des classes

--- ModelObjet/ActionAchetee.cs
+++ ModelObjet/ActionAchetee.cs
@@ -6,6 +6,10 @@ namespace ModelObjet
 {
     public class ActionAchetee
     {
-        
+        public string CodeAction { get; set; }
+        public string NomAction { get; set; }
+        public double ValeurAction { get; set; }
+        public int Quantite { get; set; }
+        public double PrixAchat { get; set; }
     }
 }
--- ModelObjet/ActionReelle.cs
+++ ModelObjet/ActionReelle.cs
@@ -4,6 +4,8 @@ namespace ModelObjet
 {
     public class ActionReelle
     {
-
+        public string CodeAction { get; set; }
+        public string NomAction { get; set; }
+        public double doubleAction { get; set; }
     }
 }
